8a489970b36d2a9c6f222b286d9f01f1e1ad9f9f
Evan <53529192+BruceMcRooster@users.noreply.github.com>
2023-11-26 16:02:23 -0500

    Initial Commit

---
+++ TreadmillRandomizer/TreadmillRandomizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreadmillRandomizerApp.swift
+//  TreadmillRandomizer
+//
+//  Created by Evan on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct TreadmillRandomizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
